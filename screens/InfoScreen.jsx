a1e5339e3c449aec727d96d4b30efd475d1f7ca8
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2024-03-12 22:22:31 +0530

    added more info

--- screens/InfoScreen.jsx
+++ screens/InfoScreen.jsx
@@ -25,14 +25,21 @@ const InfoScreen = () => {
       </View>
       <View className="flex-1 justify-center items-start w-full">
         <View className="my-2">
-          <Text className="text-white text-xl mx-2">
+          <Text className="text-center text-white text-xl mx-2">
             This app was created for learning and project purpose.
           </Text>
         </View>
         <View className="my-2">
-          <Text className="text-lg text-neutral-500 mx-2">
-            We do not store any files on our servers, all the videos are hosted
-            on 3rd party services.
+          <Text className=" text-center text-lg text-neutral-500 mx-2">
+            We do not store any files on our server. We only provide links to
+            media that is hosted on third-party services. If you have any legal
+            concerns about the free online movies on this site, you should
+            contact the actual file hosts themselves, as we are not affiliated
+            with them. We do not host or upload any videos, movies, or media
+            files. Streamify is not responsible for the accuracy,
+            compliance, copyright, legality, decency, or any other aspect of the
+            content on linked sites. If you have any legal issues, please
+            contact the appropriate media file owners or host sites.
           </Text>
         </View>
       </View>
