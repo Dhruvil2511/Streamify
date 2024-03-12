2dbfd0318bb62b1ad5bf070dc031060f52fea693
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2024-03-12 22:22:44 +0530

    fixed full screen issue

--- screens/Player.jsx
+++ screens/Player.jsx
@@ -7,6 +7,7 @@ import {
   Dimensions,
   Linking,
   Platform,
+  ToastAndroid,
   TouchableOpacity,
   View,
 } from "react-native";
@@ -23,13 +24,19 @@ const Player = () => {
   } = useRoute();
 
   const [embedString, setEmbedString] = useState(
-    `https://www.2embed.cc/embed/${id}`
+    `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1`
   );
+
+  function showToast() {
+    ToastAndroid.show("Double-tap screen for fullscreen", ToastAndroid.SHORT);
+  }
 
   useEffect(() => {
     if (id) {
       setMovieOrNot(true);
-      setEmbedString(`https://www.2embed.cc/embed/${id}`);
+      setEmbedString(
+        `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1`
+      );
     }
     if (id && episode && season) {
       setMovieOrNot(false);
@@ -48,21 +55,16 @@ const Player = () => {
     <SafeAreaView className="flex-1  bg-neutral-950">
       <WebView
         className="bg-neutral-950"
-        source={
-          movieOrNot
-            ? {
-                html: `<iframe src="${embedString}" sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" style="width: 100%; height: 100%;" frameborder="0"  allowfullscreen></iframe>`,
-              }
-            : {
-                html: `<iframe src="${embedString}"  style="width: 100%; height: 100%;" frameborder="0"  allowfullscreen></iframe>`,
-              }
-        }
+        source={{
+          html: `<iframe src="${embedString}"  style="width: 100%; height: 100%;" frameborder="0"  allowfullscreen></iframe>`,
+        }}
         limitsNavigationsToAppBoundDomains={true}
         allowsFullscreenVideo={true}
         textInteractionEnabled={false}
         showsHorizontalScrollIndicator={false}
         showsVerticalScrollIndicator={false}
         startInLoadingState
+        onLoadEnd={showToast}
       />
     </SafeAreaView>
   );
